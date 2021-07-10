3c309806b86ae5493374ac92296e9db31641c2a8
aryan0078 <aryan8931080555@gmail.com>
2021-07-10 22:22:21 +0530

    patch

--- src/commands/fun/coin.js
+++ src/commands/fun/coin.js
@@ -95,7 +95,7 @@ class Coin extends Command {
           return;
         }
 
-        let rand = await random(0, 5);
+        let rand = await random(0, 4);
         let win = false;
         //tails
         if (rand == 0 && choice == "t") win = true;
--- src/commands/social/leaderboard.js
+++ src/commands/social/leaderboard.js
@@ -46,8 +46,8 @@ class Leaderboard extends Command {
         }
 
 
-        const pos = top.findIndex(user => user.id === msg.author.id);
-        const posTxt = pos == -1 ? "??" : (top.findIndex(user => user.id === msg.author.id) + 1).toString().padStart(2, "0");
+        const pos = sorted.findIndex(user => user.id === msg.author.id);
+        const posTxt = pos == -1 ? "??" : (sorted.findIndex(user => user.id === msg.author.id) + 1).toString().padStart(2, "0");
         leaderboard.push(
           `\n+ [${posTxt}] ❯ ${msg.author.tag}\n    => ${parseInt(
             msg.member.settings.points
