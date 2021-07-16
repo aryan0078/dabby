12141efc475e77a872bba8cbf06aecd218b5643d
aryan0078 <aryan8931080555@gmail.com>
2021-07-17 01:15:14 +0530

    patch for pay

--- src/commands/social/pay.js
+++ src/commands/social/pay.js
@@ -1,82 +1,71 @@
 const Command = require("../../structures/Command.js");
 const {
-  getCurrency,
-  getCurrencyBalance,
-  withdrawBalance,
+  getdabbal,
+  paydab,
   verifyUser,
+  transactionLog,
 } = require("../../structures/database.js");
 const { replyError } = require("../../utils/constants.js");
-class Pay extends Command {
+class PayDab extends Command {
   constructor(...args) {
     super(...args, {
-      description: "Pay someone from your balance.",
-      cooldown: 5,
+      description: "Pay someone from your dab balance.",
       usage: "pay <@member> <amount>",
       guildOnly: true,
-      aliases: ["transfer", "donate", "give", "t", "g"],
+      cooldown: 5,
+      aliases: ["pay", "give", "dabpay", "dabgive", "give", "donate", "send"],
     });
   }
 
-  async run(msg, [member, amount]) {
+  async run(msg, [amount, member]) {
     member = await this.verifyMember(msg, member);
-   if (!amount) {
-     return replyError(msg, "You don't have enough dabs ");
-   }
+    if (!amount) {
+      return replyError(msg, "You don't have enough dabs ");
+    }
     amount = this.verifyInt(amount);
-    
- 
-     let db = this.client.dbClient;
-     db = await db.db();
+    let db = this.client.dbClient;
+    db = await db.db();
+    let balance = await getdabbal(msg.author.id, db);
+    let result = balance.points;
+
+    if (member.id === msg.author.id) {
+      return msg.send(" Why would you pay yourself?");
+    }
+    if (member.user.bot) {
+      return msg.send(" You can't pay bots.");
+    }
+    if (amount > parseInt(result)) {
+      return msg.send(" You can't pay more than what you have!");
+    }
+    if (amount < 1) {
+      return msg.send(" Why would you pay nothing?");
+    }
     let validation = await verifyUser(member.id, db);
     if (!validation) {
-      
       return replyError(
         msg,
-        "The user you are trying to pay is not a member dabby tell him to use \n**dab cash** \nIn order to receive cash",
+        "The user you are trying to pay is not a member dabby tell him to use \n**dab cash** \nIn order to receive cash ",
         10000
       );
     }
-    let dabs = await getCurrency(msg.guild.id, db);
-    let result = await getCurrencyBalance(msg.author.id, msg.guild.id, db);
-    try {
-      result = await getCurrencyBalance(msg.author.id, msg.guild.id, db);
-    } catch (e) {
-      return msg.send(
-        `You don't have enough ${dabs.currencyEmoji} ${dabs.currencyName}`
-      );
-    }
-    if (!result.amount) {
-      return replyError(msg, 'Please check your balance then pay', 5000)
-    }
-    if (member.id === msg.author.id)
-      return msg.send(" Why would you pay yourself?");
-    if (member.user.bot) return msg.send(" You can't pay bots.");
-    if (amount > parseInt(result.amount))
-      return msg.send(" You can't pay more than what you have!");
-    if (amount < 1) return msg.send(" Why would you pay nothing?");
-    if (amount > Number.MAX_SAFE_INTEGER)
-      return msg.send(" That amount is too high!");
-
     await member.syncSettings();
     try {
-     
-        await withdrawBalance(msg.author.id, msg.guild.id, amount, true, db);
-     
-      
-      await withdrawBalance(member.id, msg.guild.id, amount, false, db);
+      // await paydab(msg.author.id, member.id, amount, msg.guild.id, db);
+      member.givePoints(amount);
+      msg.member.takePoints(amount);
+      await transactionLog(amount, msg.author.id, member.id, msg.guild.id, db);
     } catch (err) {
       return msg.send(
-        `Unable to pay to **${member.displayName}** is not registered member of dabby \n or you don't have enough ${result.currencyEmoji} use dab convert <amount> to convert your dabs`
+        `Unable to pay to **${member.displayName}** is not registered member of dabby \n or you don't have enough <:dabs:851218687255773194> use dab reswap <amount> to convert your dabs`
       );
     }
-    
 
     return msg.send(
-      `Paid **${amount.toLocaleString()}** ${dabs.currencyName} ${
-        dabs.currencyEmoji
-      } to **${member.displayName}**`
+      `**${msg.author.username
+      } ${await this.beta(msg) ? this.betaemoji : ''} |**Paid <:dabs:851218687255773194> **${amount.toLocaleString()}** dabs to **${member.displayName
+      }**`
     );
   }
 }
 
-module.exports = Pay;
+module.exports = PayDab;
