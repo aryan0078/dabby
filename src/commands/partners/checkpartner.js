911a844ddf2f9cc8af0313dd6efc32ebb3195085
aryan0078 <aryan8931080555@gmail.com>
2021-07-16 18:27:55 +0530

    patch

--- src/commands/partners/checkpartner.js
+++ src/commands/partners/checkpartner.js
@@ -17,34 +17,29 @@ class CheckPartner extends Command {
 
         let db = this.client.dbClient;
         db = await db.db();
-        if (!await this.globalpartnercheck(msg)) {
+        if (await this.globalpartnercheck(msg) || await this.workercheck(msg) || await this.guidercheck(msg)) {
+            let user = await this.verifyMember(msg, msg.author.user, true);
+
+            let u = await db.collection("partners");
+            let f = `Partners for **${msg.guild.name}** Please check time carefully then verify otherwise we have to take actions\nIn DM\n`
+            let p = await u.find({ server: msg.guild.id }).sort({ at: 1 }).toArray()
+            p.forEach(async (element, index) => {
+
+
+                let user = await this.client.users.fetch(element.id)
+
+                f += `**${user.username}** at ${new Date(element.at)}\n`
+            });
+
+            return msg.send(f).then(ms => {
+                user.send(f);
+            })
+        }
+        else {
+
+
             return replyError(msg, 'This command is only for global partners,partners,workers,guiders', 5000)
         }
-        if (!await this.workercheck(msg)) {
-            return replyError(msg, 'This command is only for global partners,partners,workers,guiders', 5000)
-        }
-        if (!await this.guidercheck(msg)) {
-            return replyError(msg, 'This command is only for global partners,partners,workers,guiders', 5000)
-        }
-        let user = await this.verifyMember(msg, msg.author.user, true);
-
-        let u = await db.collection("partners");
-        let f = `Partners for **${msg.guild.name}** Please check time carefully then verify otherwise we have to take actions\nIn DM\n`
-        let p = await u.find({ server: msg.guild.id }).sort({ at: 1 }).toArray()
-        p.forEach(async (element, index) => {
-
-
-            let user = await this.client.users.fetch(element.id)
-
-            f += `**${user.username}** at ${new Date(element.at)}\n`
-        });
-
-        return msg.send(f).then(ms => {
-            user.send(f);
-        })
-
-
-
 
 
 
