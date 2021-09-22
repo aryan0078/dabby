17a385ac4752f8d3dc7ed9ca546c4ce2223e6e43
Aryan Singh <44253539+aryan0078@users.noreply.github.com>
2021-09-22 22:00:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./index.js', { token: 'Nzg0NzE3NjgzNDU0Mzc4MDI0.X8tXSw.plimjGKyzj_6FyeRh4BVtESyF5k', totalShards: "auto" });
+const manager = new ShardingManager('./index.js', { token: 'INSERT TOKEN HERE', totalShards: "auto" });
 
 manager.on('shardCreate', shard => {
     console.log(`Launched shard ${shard.id}`);
